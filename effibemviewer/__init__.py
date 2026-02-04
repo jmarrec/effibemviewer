e5460e4a31ae0f49143c8e0bdcce3d192d4ae66a
Julien Marrec <julien.marrec@gmail.com>
2026-02-04 23:57:51 +0100

    Add pre commit

--- effibemviewer/__init__.py
+++ effibemviewer/__init__.py
@@ -1,9 +1,4 @@
-from effibemviewer.gltf import (
-    display_model,
-    model_to_gltf_html,
-    model_to_gltf_json,
-    model_to_gltf_script,
-)
+from effibemviewer.gltf import display_model, model_to_gltf_html, model_to_gltf_json, model_to_gltf_script
 
 __all__ = [
     "display_model",
